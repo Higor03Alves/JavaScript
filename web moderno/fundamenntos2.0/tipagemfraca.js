88fd1ef83403fc2120fa2a41527ba6c51cb70229
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-10-14 14:37:07 -0300

    numeros

---
+++ web moderno/fundamenntos2.0/tipagemfraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'Olá mundo';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1415;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//evitar nomes genericos e siglas
+let valor = ''
+let  numero =  
+let P =   ''// pqp = produto quimicos perigosos
+// evitar 100% ESSE TIPO DE NOME PARA VARIAVEIS
