d3ca6031ec927766c103d0382ebd411a21a3c3b0
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-10-14 20:43:08 -0300

    string

--- web moderno/fundamenntos2.0/cuidadocomnumeros.js
+++ web moderno/fundamenntos2.0/cuidadocomnumeros.js
@@ -1,6 +1,7 @@
 console.log(7/0);
-console.log('10'/2)
-console.log("show!!" * 2)
-console.log(0.1 + 0.7)
+console.log('3' +  2); // usando o sinal de mais e a string ganha preferencia
+console.log('10'/2);
+console.log("show!!" * 2);
+console.log(0.1 + 0.7);
 //console.log(10.toFixed(2)) vai da erro
-console.log((10).toFixed(2))
+console.log((10).toFixed(2));
---
+++ web moderno/fundamenntos2.0/string.js
@@ -0,0 +1,13 @@
+const escola ='alv4s';
+
+console.log(escola.charAt(4));
+console.log(escola.charAt(8));
+console.log(escola.charCodeAt(3));
+console.log(escola.indexOf('4'));
+
+console.log(escola.substring(1));
+console.log(escola.substring(0,3));
+
+console.log('escola '.concat(escola).concat("!"))
+console.log(escola.replace(4, 'e'))
+console.log('higor,higor,higor'.split(','))
