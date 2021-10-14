a4da3ed431a23855a81c7e3f35e61794bfba3490
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-10-14 13:53:30 -0300

    variavel e constante

---
+++ web moderno/fundamenntos2.0/variavelecontante.js
@@ -0,0 +1,15 @@
+var a = 3;
+let b = 4;
+
+var a = 30;
+b = 40;
+
+console.log(a,b);
+
+a =200;
+b =300;
+console.log(a,b);
+
+const c = 5;
+// c = 50 em constante o valor nao pode ser modificado
+console.log(c);
