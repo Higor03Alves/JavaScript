eac30790135251c4c92bc15514cb67b1a24b6ac3
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-10-15 12:50:21 -0300

    objetos

---
+++ web moderno/fundamenntos2.0/object.js
@@ -0,0 +1,13 @@
+const prod1 = {};
+prod1.nome = 'Celular s10 Ultra'
+prod1.valor = 1259.99
+prod1['Desconto de'] = 0.40 // evitar atributos com espaco
+
+console.log(prod1);
+
+const prod2 = {
+    nome : 'iphone',
+    preco : 3999.99,
+    desconto : 0.18
+};
+console.log(prod2)
