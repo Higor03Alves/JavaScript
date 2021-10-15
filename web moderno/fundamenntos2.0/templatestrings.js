cfd128b8f32899f56b4b31fcd5ff70c30d3384d2
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-10-14 21:07:37 -0300

    template strings

---
+++ web moderno/fundamenntos2.0/templatestrings.js
@@ -0,0 +1,11 @@
+ const nome = 'Dino';
+ const concatenacao = 'Olá '  + nome + '!';
+const template = `
+Olá
+${nome}`;
+console.log(concatenacao, template);
+
+//exprecoes ....
+console.log(`1 + 1 = ${1 + 1}`)
+const up = texto => texto.toUpperCase()
+console.log(`Você ai...${up('corre')}!!!!`)
