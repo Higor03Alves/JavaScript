e9ba3dbe8e63d9ac385eebf5823650717e157cdb
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-10-15 10:44:13 -0300

    Update booleanos.js

--- web moderno/fundamenntos2.0/booleanos.js
+++ web moderno/fundamenntos2.0/booleanos.js
@@ -10,6 +10,17 @@ console.log(!isAtivo);
 
 console.log(' os verdadeiros');
 console.log(!!6);
+console.log(!!7);
+console.log(!!-8);
+console.log(!!'verdadeiro ');
+console.log(!![]);
+console.log(!!Infinity);
+console.log(!!(isAtivo = true));
+
+console.log('os falsos');
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
 console.log(!!NaN);
 console.log(!!undefined);
 console.log(!!(isAtivo = false));
