b09554a919b8fb7c7ded175b54bb2d63a6306c8f
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-10-22 15:14:07 -0300

    operadores

---
+++ web moderno/fundamenntos2.0/operadores.js
@@ -0,0 +1,13 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const soma = a + b + c + d
+const subtracao = d - c - b - a
+const multiplicacao = a * b * c * d
+const divisao = d / c / b / a
+const modulo = a % 2
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(-divisao);
+console.log(modulo);
