edcc6f2839ecaa38d49bf8ed82bb6cce573c4e8e
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-10-27 13:50:58 -0300

    function

---
+++ web moderno/funcoes/thisEbind01.js
@@ -0,0 +1,16 @@
+const pessoa = {
+    saudacao: 'bom dia!',
+    falar() {
+        console.log(this.saudacao)
+    }
+};
+
+pessoa.falar()
+const falar = pessoa.falar
+falar(); // conflito entre paradigmas : funcional e OO
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa();
+
+const falar2 = pessoa.falar;
+falar2();
