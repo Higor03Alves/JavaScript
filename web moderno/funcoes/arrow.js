2b66ea271c81aa1107ec0c8b26b7e82621076b5c
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-10-28 09:46:26 -0300

    funcoes

---
+++ web moderno/funcoes/arrow.js
@@ -0,0 +1,18 @@
+ let mult = function (a) {
+    return 2 * a
+ };
+
+mult = (a) => {
+    return 2 * a 
+};
+
+mult = a => 2 * a
+console.log(mult(Math.PI));
+
+let ola = function () {
+    return 'Olá mundo'
+};
+
+ola = () => 'Olá mundo';
+ola =_=> 'Olá mundo'; // possui um parametro
+console.log(ola());
