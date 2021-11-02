0fa629068fc7979a42123eaa7c6605b263cc7b15
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-11-02 14:14:17 -0300

    oo

--- web moderno/Orientação a Objetos/oo.js
+++ web moderno/Orientação a Objetos/oo.js
@@ -1,3 +1,16 @@
 // CODIGOS NÃO EXECULTÁVEIS!!!!
 
-//Procedural
+// Procedural
+processamento(valor1, valor2, valor3);
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+};
+
+objeto.processamento();
