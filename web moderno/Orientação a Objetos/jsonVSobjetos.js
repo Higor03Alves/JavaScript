473b22bbd0bf70840293956dfc661ebc678ffc9b
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-11-05 13:04:55 -0300

    JSON

---
+++ web moderno/Orientação a Objetos/jsonVSobjetos.js
@@ -0,0 +1,8 @@
+// Trasformando um objeto em um JSON
+const obj = { a: 1, b: 2, c: 3, soma () {return a + b + c} };
+console.log(JSON.stringify(obj));
+
+//console.log(JSON.parse("{ a: 1, b: 2, c:3}")); assim nao vai funcionar!
+//console.log(JSON.parse("{ 'a': 1, 'b': 2, 'c':3}")); assim nao vai funcionar!
+console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3}'));
+console.log(JSON.parse('{ "a": 1, "b": "string", "c": true, "d": {}, "e": [] }'));
