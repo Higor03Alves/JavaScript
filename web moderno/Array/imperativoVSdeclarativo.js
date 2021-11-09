3c1bb0cb5680d9f3db37ada72726f725d7171e04
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-11-09 15:35:40 -0300

    arrays

--- web moderno/Array/imperativoVSdeclarativo.js
+++ web moderno/Array/imperativoVSdeclarativo.js
@@ -16,4 +16,4 @@ const getNota = aluno => aluno.nota;
 const soma = (total, atual) => total + atual;
 
 const total2 = alunos.map(getNota).reduce(soma);
-console.log(total2 / alunos.length)
+console.log(total2 / alunos.length);
