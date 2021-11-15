ae792d04a5421e6513a48e117edaa9e515cfc387
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-11-15 15:33:41 -0300

    require

---
+++ web moderno/Node/PastaA/PastaB/acessarModulo.js
@@ -0,0 +1,11 @@
+const moduloA = require('../../moduloA');
+console.log(moduloA.ola);
+
+const c = require('./PastaC');
+console.log(c.ola2)
+
+//const http = require('http');
+//http.createServer((req, res) => {
+//   req.write('Bom dia!!')
+//    res.end()
+//}).listen(8080)
