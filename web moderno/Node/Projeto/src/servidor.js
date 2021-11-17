3e87b4db06e246bd79b757b649d5e745237e5737
Higor Alves Duarte <89613962+Higor03Alves@users.noreply.github.com>
2021-11-17 14:37:06 -0300

    continuação do projeto com |Express

--- web moderno/Node/Projeto/src/servidor.js
+++ web moderno/Node/Projeto/src/servidor.js
@@ -2,10 +2,23 @@ const porta = 3003;
 
 const express = require('express');
 const app = express();
+const bancoDeDados = require('./bancoDeDados')
 
 
 app.use('/produtos',(req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45}) // converter para o formato JSON
+    res.send(bancoDeDados.getProdutos())
+});
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+});
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
 });
 
 app.listen(porta, () => {
